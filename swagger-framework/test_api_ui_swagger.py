abee9d9eadaf48454ada83c658a8c2b1f56861e0
mikalai-shkuratovich <nikolayshkuratovich@gmail.com>
2024-03-06 13:07:31 +0100

    corrections to conftest

--- swagger-framework/test_api_ui_swagger.py
+++ swagger-framework/test_api_ui_swagger.py
@@ -1,55 +1,51 @@
-from swagger_playwright import SecretKey, BasePage
-from api_framework_swagger import RequestPOST, RequestGET, RequestPUT, RequestDELETE
+# def test_create_and_verify(browser_session, random_name):
+#     """This test create new record by POST method, afterwards GET
+#     method takes records data and verify equality of the names"""
+
+#     page = browser_session
+#     secret_key = SecretKey.get_secret_key(page)
+#     endpoint = BasePage.endpoint_coffee(secret_key)
+#     coffee_name = random_name
+
+#     payload = {"Name": {coffee_name}, "Description": "Never be the same!"}
+#     record_id = RequestPOST.post_create_record(endpoint, payload)
+#     record_name = RequestGET.get_record_data(endpoint, record_id)
+#     assert coffee_name == record_name
 
 
-def test_create_and_verify(browser_session, random_name):
-    """This test create new record by POST method, afterwards GET
-    method takes records data and verify equality of the names"""
+# def test_create_and_update(browser_session):
+#     """This test create new record by POST method, afterwards PUT
+#     method will update Name of the Coffee"""
 
-    page = browser_session
-    secret_key = SecretKey.get_secret_key(page)
-    endpoint = BasePage.endpoint_coffee(secret_key)
-    coffee_name = random_name
+#     page = browser_session
+#     secret_key = SecretKey.get_secret_key(page)
+#     endpoint = BasePage.endpoint_coffee(secret_key)
 
-    payload = {"Name": {coffee_name}, "Description": "Never be the same!"}
-    record_id = RequestPOST.post_create_record(endpoint, payload)
-    record_name = RequestGET.get_record_data(endpoint, record_id)
-    assert coffee_name == record_name
+#     payload_post = {"Name": "New Comfy Coffee", "Description": "Never be the same!"}
+#     record_id = RequestPOST.post_create_record(endpoint, payload_post)
 
-
-def test_create_and_update(browser_session):
-    """This test create new record by POST method, afterwards PUT
-    method will update Name of the Coffee"""
-
-    page = browser_session
-    secret_key = SecretKey.get_secret_key(page)
-    endpoint = BasePage.endpoint_coffee(secret_key)
-
-    payload_post = {"Name": "New Comfy Coffee", "Description": "Never be the same!"}
-    record_id = RequestPOST.post_create_record(endpoint, payload_post)
-
-    payload_put = {
-        "Id": record_id,
-        "Name": "Updated new Name",
-        "Description": "Be the same!",
-    }
-    updated_name = RequestPUT.put_update_record(endpoint, payload_put)
-    record_name = RequestGET.get_record_data(endpoint, record_id)
-    assert updated_name == record_name
+#     payload_put = {
+#         "Id": record_id,
+#         "Name": "Updated new Name",
+#         "Description": "Be the same!",
+#     }
+#     updated_name = RequestPUT.put_update_record(endpoint, payload_put)
+#     record_name = RequestGET.get_record_data(endpoint, record_id)
+#     assert updated_name == record_name
 
 
-def test_create_and_delete(browser_session):
-    """This test create new record by POST method, afterwards DELETE
-    method will delete this record"""
+# def test_create_and_delete(browser_session):
+#     """This test create new record by POST method, afterwards DELETE
+#     method will delete this record"""
 
-    page = browser_session
-    secret_key = SecretKey.get_secret_key(page)
-    endpoint = BasePage.endpoint_coffee(secret_key)
+#     page = browser_session
+#     secret_key = SecretKey.get_secret_key(page)
+#     endpoint = BasePage.endpoint_coffee(secret_key)
 
-    payload_post = {"Name": "New Comfy Coffee", "Description": "Never be the same!"}
-    record_id = RequestPOST.post_create_record(endpoint, payload_post)
-    delete_record = RequestDELETE.delete_record(endpoint, record_id)
-    assert delete_record == 204
+#     payload_post = {"Name": "New Comfy Coffee", "Description": "Never be the same!"}
+#     record_id = RequestPOST.post_create_record(endpoint, payload_post)
+#     delete_record = RequestDELETE.delete_record(endpoint, record_id)
+#     assert delete_record == 204
 
 
 # @pytest.mark.skip
@@ -59,7 +55,6 @@ def test_log_in(browser_session):
     page.set_default_timeout(30000)
 
     page.locator("xpath=//*[contains(@class, 'jjPduP')]").click()
-    # page.click("'Log In'")
     # page.get_by_role("button", name="Log In").click()
     # page.get_by_role(role="button", name="Log In").click(timeout=3000)
     # page.locator("xpath=//*[contains(@data-testid, 'buttonElement')]").click()
